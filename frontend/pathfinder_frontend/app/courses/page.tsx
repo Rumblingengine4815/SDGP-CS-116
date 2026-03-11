3e63b4bdd396325c03e5ea05a6ea058dea32c325
Nithila Mandiw <nithilamandiw@Nithilas-MacBook-Air.local>
2026-03-11 19:38:33 +0530

    add filter icon to courses page

--- frontend/pathfinder_frontend/app/courses/page.tsx
+++ frontend/pathfinder_frontend/app/courses/page.tsx
@@ -1,11 +1,24 @@
+import Image from "next/image";
+
 export default function Courses() {
 
   return (
     <div className="min-h-screen bg-gray-50 px-16 py-10">
 
-      <h1 className="text-3xl font-semibold text-gray-800">
-        Suggested Courses
-      </h1>
+      <div className="flex justify-between items-center mb-8">
+
+        <h1 className="text-3xl font-semibold text-gray-800">
+          Suggested Courses
+        </h1>
+
+        <Image
+          src="/images/filter.png"
+          alt="filter"
+          width={30}
+          height={30}
+        />
+
+      </div>
 
     </div>
   );
