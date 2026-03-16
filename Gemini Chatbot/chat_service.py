9fe9023a65d24c74553652ae69fbb3ac30e56b5a
thathsara336 <thathsara.20241311@iit.ac.lk>
2026-03-16 11:04:55 +0530

    Update chat_service.py by adding initial class

--- Gemini Chatbot/chat_service.py
+++ Gemini Chatbot/chat_service.py
@@ -1,1 +1,9 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+class ChatService:
+    def __init__(self, db=None):
+        self.db = db          #  MongoDB
+        self._setup_gemini()
 
